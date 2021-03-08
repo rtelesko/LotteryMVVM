0a99bdcaa34b9ab5bed5a17af82b7edf92d4c951
rainer.telesko <rainer.telesko@fhnw.ch>
2021-03-08 10:26:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lotterymvvm/AppViewModel.java
@@ -0,0 +1,70 @@
+package com.example.lotterymvvm;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class AppViewModel extends BaseObservable {
+
+    // creating object of Model class
+    private Model model;
+
+    // string variables for toast messages
+    private String successMessage = "Login successful";
+    private String errorMessage = "Username or Password is not valid";
+
+    @Bindable
+    // string variable for toast message
+    private String toastMessage = null;
+
+    // constructor of ViewModel class
+    public AppViewModel() {
+
+        // instantiating object of model class
+        model = new Model("", "");
+    }
+
+    // getter and setter methods for toast message
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+    private void setToastMessage(String toastMessage) {
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    // getter and setter methods for username variable
+    @Bindable
+    public String getUsername() {
+        return model.getUsername();
+    }
+
+    public void setUsername(String username) {
+        model.setUsername(username);
+        notifyPropertyChanged(BR.username);
+    }
+
+    // getter and setter methods for password variable
+    @Bindable
+    public String getUserPassword() {
+        return model.getPassword();
+    }
+
+    public void setUserPassword(String password) {
+        model.setPassword(password);
+        notifyPropertyChanged(BR.userPassword);
+    }
+
+    // actions to be performed when user clicks the LOGIN button
+    public void onButtonClicked() {
+        if (isValid())
+            setToastMessage(successMessage);
+        else
+            setToastMessage(errorMessage);
+    }
+
+    // check for validity
+    public boolean isValid() {
+        return getUsername().length() >= 5 && getUserPassword().equals("admin");
+    }
+}
